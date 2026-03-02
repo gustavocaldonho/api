2b86a5b090b165a0c2197678ad5501e88cfc3e8b
gustavocaldonho <gcaldonho3@email.com>
2026-03-02 11:45:13 -0300

    Applying name search to the 'listar_clientes' route

--- routes/client_routes.py
+++ routes/client_routes.py
@@ -9,7 +9,8 @@ from typing import List
 client_router = APIRouter(prefix="/client", tags=["clientes"])
 
 @client_router.get("/listar_clientes/{empresa_id}", response_model=List[VisualizarClientesSchema])
-async def listar_clientes(empresa_id: int, page: int = Query(1, ge=1), size: int = Query(10, ge=1, le=100), 
+async def listar_clientes(empresa_id: int, nome_cliente: str | None = Query(None, min_length=3, max_length=50), 
+                          page: int = Query(1, ge=1), size: int = Query(10, ge=1, le=100), 
                           session: Session = Depends(pegar_sessao)):
     # usuario: Usuarios_Integra = Depends(verificar_token)
     # Query(valor_padrao, greater_or_equal 'ge' (maior ou igual), less_than_or_equal 'le' (menor ou igual))
@@ -20,7 +21,12 @@ async def listar_clientes(empresa_id: int, page: int = Query(1, ge=1), size: int
     
     skip = (page-1)*size
     # se o vendedor possuir autorização, ele consulta os clientes
-    clientes = session.query(Pessoas).filter(Pessoas.empresa_id==empresa_id).order_by(Pessoas.nome).offset(skip).limit(size).all()
+    query =  session.query(Pessoas).filter(Pessoas.empresa_id==empresa_id)
+    # filtrando os clientes pelo nome se o usuário digitar
+    if nome_cliente:
+        query = query.filter(Pessoas.nome.ilike(f"%{nome_cliente}%"))
+    # ordenando os nomes e aplicando a paginação
+    clientes = query.order_by(Pessoas.nome).offset(skip).limit(size).all()
     if not clientes:
         raise HTTPException(status_code=400, detail="Não foram encontrados clientes para a referida empresa (empresa_id)")
     else:
